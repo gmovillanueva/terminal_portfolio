6c32a3b6c813ac7095edfd702eb288da44d373e3
G-mo V. <gmow.villanueva@gmail.com>
2024-02-02 11:55:51 -0600

    Added GUI finally.

--- src/components/Commands/GUI.tsx
+++ src/components/Commands/GUI.tsx
@@ -1,13 +1,18 @@
+import { useEffect } from 'react';
+
 const GUI = (args: string[]) => {
   if (args.length > 0)
     return <p>gui: {args.join(' ')} is not a valid command.</p>;
+
+  useEffect(() => {
+    window.location.href = 'https://gui.willnewton.tech/';
+  }, []);
+
   return (
     <div
       className='mb-3 ml-5 mt-1'
       data-testid='gui'
-    >
-      <p className='mt-2 leading-6'>Work in progress!</p>
-    </div>
+    ></div>
   );
 };
 
